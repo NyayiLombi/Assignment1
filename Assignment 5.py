15c86066e3d2c8a399d0b8c605fcd6f31d8b9a89
NyayiLombi <103368655+NyayiLombi@users.noreply.github.com>
2022-06-18 19:16:56 +0530

    Add files via upload

---
+++ Assignment 5.py
@@ -0,0 +1,123 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+Ans 1)
+Enter string: werth
+The original string  is :  werth
+The reversed string using a while loop is :  htrew
+
+Ans 2)
+Enter the number: 2
+Enter the range: 23
+The numbers divisible are: 
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+
+Ans3
+Enter first side: 6
+Enter second side: 8
+Enter third side: 10
+The area of the triangle is  24.0
+
+Ans4
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+Ans5
+number of rows: 4
+A
+BC
+DEF
+GHIJ
+
+Ans6
+Please, Enter the Lowest Range Value: 1
+Please, Enter the Upper Range Value: 10
+The Prime Numbers in the range are: 
+2
+3
+5
+7
+
+Ans7
+77
+154
+231
+308
+385
+462
+
+Ans8
+Enter value: 2
+Enter value: 3
+Enter value: 7
+Enter value: 8
+Enter value: -1
+Enter value: -3
+Enter value: -4
+Enter value: 2
+Enter value: -1
+Enter value: 8
+positive numbers
+2
+3
+7
+8
+2
+8
+negative numbers
+-1
+-3
+-4
+-1
+odd numbers
+3
+7
+-1
+-3
+-1
+even numbers
+2
+8
+-4
+2
+8
+Number of times each number occurs in the List
+Count of 2 = 2
+Count of 3 = 1
+Count of 7 = 1
+Count of 8 = 2
+Count of -1 = 2
+Count of -3 = 1
+Count of -4 = 1
+Count of 2 = 2
+Count of -1 = 2
+Count of 8 = 2
+
+Ans9
+
+length of list: 4
+Enter value: wer
+Enter value: wer
+Enter value: ter
+Enter value: ret
+Count of wer = 2
+Count of wer = 2
+Count of ter = 1
