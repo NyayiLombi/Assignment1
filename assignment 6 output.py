bf24d351f7d87fcdd6beabba6e56622c6c456852
NyayiLombi <103368655+NyayiLombi@users.noreply.github.com>
2022-07-04 10:12:01 +0530

    Add files via upload

---
+++ assignment 6 output.py
@@ -0,0 +1,62 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/asus/AppData/Local/Programs/Python/Python310/Assignment 6.py
+
+Welcome to my Program
+28
+(True, True)
+
+Welcome to my Program
+Enter a word : radar
+Yes, word is a palindrome
+
+Welcome to my Program
+      1 
+
+     1 1 
+
+    1 2 1 
+
+   1 3 3 1 
+
+  1 4 6 4 1 
+
+
+Welcome to my Program
+
+Enter a string : the quick brown fox jumps over the lazy dog
+True
+
+Welcome to my Program
+Enter the elements : green-red-yellow-black-white
+
+Sorted elements : 
+black-green-red-white-yellow
+
+Welcome to my Program
+
+Student ID: 21102132
+Student Name : Nyayi Lombi
+
+Student ID: 21102132
+Student Name : Nyayi Lombi
+Student Name : Nyayi Lombi
+Student Class : XII
+
+Welcome to my Program
+To check whether they are instances of said classes or not:
+True
+False
+True
+False
+
+To check whether the said classes are subclasses of the built-in object class or not:
+True
+True
+
+Welcome to my Program
+[[-10, 2, 8], [-7, -3, 10]]
+
+Welcome to my Program
+False
